8daabcb9fed4fa585e65392f5f6576e97d48dfdc
sean-brandenburg <seanbrandenburg333@gmail.com>
2019-04-13 15:14:06 -0400

    Updated config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-twitConsumerKey = '1bd1NSKem1aiZfqnWUcH5ViuP'
-twitConsumerSecret = 'QMphn82ArjoeyqlyH2fEGISqefTug3AWWr8Vfe6qwb2WGxxIZF'
-token = '760743818618662912-rZgyYy2wgIyL2JqiJM4kO5ZUtBlQ32D'
-tokenSecret = '7gckswDBRdyWTs3fFHuQgxmxMspNrGKAC595DP4R4TEoS'
+twitConsumerKey = 'UkYb0CVW9sJcVdTZcMA0pGm36'
+twitConsumerSecret = 'LIfL3jrYUbtPOPQQIcWOxnHp7eHdwYYxNu2CjUMgRg4SoEjgzJ'
+token = '1117084223519719424-Tplnpjr1NAqYWhlzBgNgBNQfWclTHq'
+tokenSecret = 'OsthXNYucRZwS9XNuYeqtvtpRZQDm7V0StPdFjnWdx5N7'
