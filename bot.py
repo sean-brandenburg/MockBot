d0feb269ad9d8c241260837600fd669f8d280e2b
d-philip <djphilip@bu.edu>
2019-04-13 14:00:33 -0400

    Moved newfile code to `main.py` and `bot.py`.

--- bot.py
+++ bot.py
@@ -1,70 +1,71 @@
 import re
-import config
 import tweepy
-import bot
+import config
 from tweepy import OAuthHandler
 from textblob import TextBlob
-from mock import mockText
 
-class TwitterClient:
-    def __init__self(self):
+class TwitterClient(object):
+	def __init__(self):
+		# keys and tokens from the Twitter Dev Console
+		consumer_key = config.twitConsumerKey
+		consumer_secret = config.twitConsumerSecret
+		access_token = config.token
+		access_token_secret = config.tokenSecret
 
-        consumer_key = config.twitConsumerKey
-        consumer_secret = config.twitConsumerSecret
-        access_token = config.token
-        access_token_secret = config.tokenSecret
+		# attempt authentication
+		try:
+			# create OAuthHandler object
+			self.auth = OAuthHandler(consumer_key, consumer_secret)
+			# set access token and secret
+			self.auth.set_access_token(access_token, access_token_secret)
+			# create tweepy API object to fetch tweets
+			self.api = tweepy.API(self.auth)
+		except:
+			print("Error: Authentication Failed")
 
-        # attempt authentication
-        try:
-            # create OAuthHandler object
-            self.auth = OAuthHandler(consumer_key, consumer_secret)
-            # set access token and secret
-            self.auth.set_access_token(access_token, access_token_secret)
-            # create tweepy API object to fetch tweets
-            self.api = tweepy.API(self.auth)
-        except:
-            print("Error: Authentication Failed")
+	def clean_tweet(self, tweet):
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
-    def get_tweet_sentiment(self, tweet):
-        # create TextBlob object of passed tweet text
-        analysis = TextBlob(self.clean_tweet(tweet))
-        # set sentiment
-        if analysis.sentiment.polarity > 0:
-            return 'positive'
-        elif analysis.sentiment.polarity == 0:
-            return 'neutral'
-        else:
-            return 'negative'
+	def get_tweet_sentiment(self, tweet):
+		# create TextBlob object of passed tweet text
+		analysis = TextBlob(self.clean_tweet(tweet))
+		# set sentiment
+		if analysis.sentiment.polarity > 0:
+			return 'positive'
+		elif analysis.sentiment.polarity == 0:
+			return 'neutral'
+		else:
+			return 'negative'
 
-    def get_tweets(self, query, count = 10):
-    # empty list to store parsed tweets
-        tweets = []
+	def get_tweets(self, query, count = 10):
+		# empty list to store parsed tweets
+		tweets = []
 
-        try:
-            # call twitter api to fetch tweets
-            fetched_tweets = self.api.search(q = query, count = count)
+		try:
+			# call twitter api to fetch tweets
+			fetched_tweets = self.api.search(q = query, count = count)
 
-            # parsing tweets one by one
-            for tweet in fetched_tweets:
-                # empty dictionary to store required params of a tweet
-                parsed_tweet = {}
+			# parsing tweets one by one
+			for tweet in fetched_tweets:
+				# empty dictionary to store required params of a tweet
+				parsed_tweet = {}
 
-                # saving text of tweet
-                parsed_tweet['text'] = tweet.text
-                # saving sentiment of tweet
-                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+				# saving text of tweet
+				parsed_tweet['text'] = tweet.text
+				# saving sentiment of tweet
+				parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
 
-                # appending parsed tweet to tweets list
-                if tweet.retweet_count > 0:
-                    # if tweet has retweets, ensure that it is appended only once
-                    if parsed_tweet not in tweets:
-                        tweets.append(parsed_tweet)
-                else:
-                    tweets.append(parsed_tweet)
+				# appending parsed tweet to tweets list
+				if tweet.retweet_count > 0:
+					# if tweet has retweets, ensure that it is appended only once
+					if parsed_tweet not in tweets:
+						tweets.append(parsed_tweet)
+				else:
+					tweets.append(parsed_tweet)
 
-            # return parsed tweets
-            return tweets
+			# return parsed tweets
+			return tweets
 
-        except tweepy.TweepError as e:
-            # print error (if any)
-            print("Error : " + str(e))
+		except tweepy.TweepError as e:
+			# print error (if any)
+			print("Error : " + str(e))
