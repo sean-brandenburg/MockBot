fe2a255f603516eb2310ae55e78ed0f434b40af0
d-philip <djphilip@bu.edu>
2019-04-13 12:08:55 -0400

    Hiding files.

--- config.py
+++
@@ -1,4 +0,0 @@
-twitConsumerKey = '1bd1NSKem1aiZfqnWUcH5ViuP'
-twitConsumerSecret = 'QMphn82ArjoeyqlyH2fEGISqefTug3AWWr8Vfe6qwb2WGxxIZF'
-token = '760743818618662912-rZgyYy2wgIyL2JqiJM4kO5ZUtBlQ32D'
-tokenSecret = '7gckswDBRdyWTs3fFHuQgxmxMspNrGKAC595DP4R4TEoS'
