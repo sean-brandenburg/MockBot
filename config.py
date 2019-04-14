186324184853b2cbd8298ce29024fc242860671c
sean-brandenburg <seanbrandenburg333@gmail.com>
2019-04-13 22:22:32 -0400

    Added comments to config file

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-twitConsumerKey = 'UkYb0CVW9sJcVdTZcMA0pGm36'
-twitConsumerSecret = 'LIfL3jrYUbtPOPQQIcWOxnHp7eHdwYYxNu2CjUMgRg4SoEjgzJ'
-token = '1117084223519719424-Tplnpjr1NAqYWhlzBgNgBNQfWclTHq'
-tokenSecret = 'OsthXNYucRZwS9XNuYeqtvtpRZQDm7V0StPdFjnWdx5N7'
+twitConsumerKey = #Add Key Here
+twitConsumerSecret = #Add Key Here
+token = #Add Key Here
+tokenSecret = #Add Key Here
